6675137b00707b3120dd78e9223790aeb64e7c7b
yusufalibrahim1994 <57629778+yusufalibrahim1994@users.noreply.github.com>
2019-12-06 19:07:12 +0300

    Initial commit

---
+++ Assets/Scripts/script_mpControls.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class script_mpControls : MonoBehaviour
+{
+    Vector2 i_movement;
+    [SerializeField] float moveSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 movement = new Vector3(i_movement.x, 0, i_movement.y) * moveSpeed * Time.deltaTime;
+        transform.Translate(movement);
+    }
+
+    void OnMove(InputValue value)
+    {
+        i_movement = value.Get<Vector2>();
+    }
+
+    void OnMoveUp()
+    {
+        transform.Translate(transform.up);
+    }
+
+    void OnMoveDown()
+    {
+        transform.Translate(-transform.up);
+    }
+}
